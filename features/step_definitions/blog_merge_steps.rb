d7d1e9d3c3e5017fe51706f4d3bd449e760b515f
jonkim93 <jonkim93@gmail.com>
2015-03-11 17:35:17 -0700

    implemented some steps + some cucumber stuff

---
+++ features/step_definitions/blog_merge_steps.rb
@@ -0,0 +1,51 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
+Given /^this user exists:$/ do |fields| 
+	#User.create!(Hash[fields.map{ |k, v| [k.to_sym, v] }])
+	params = {}
+	fields.rows_hash.each do |name, value|
+		params[name.to_sym] = value
+	end
+	User.create!(params)
+end
+
+Given /^this article exists:$/ do |fields|
+	params = {}
+	fields.rows_hash.each do |name, value|
+		if name=='user'
+			params[name.to_sym] = User.where(:login=>value).first
+		else
+			params[name.to_sym] = value
+		end
+	end
+	Article.create!(params)
+end
+
+Given /^this comment exists:$/ do |fields|
+	params = {}
+	fields.rows_hash.each do |name, value|
+		if name=='user'
+			params[name.to_sym] = User.where(:login=>value).first
+		elsif name=='article'
+			params[name.to_sym] = Article.where(:title=>value).first
+		else
+			params[name.to_sym] = value
+		end
+	end
+	Comment.create!(params)
+end
+
+When /^I edit "([^"]*)"$/ do |page_name|
+	id = Article.where(:title=>page_name).first.guid
+	edit_uri = 'admin/content/edit/#{id}'
+	visit edit_uri
+end
+
+When /^I merge with "([^"]*)"$/ do |article_name|
+	id = Article.where(:title=>article_name).guid
+	step %Q|I fill in "merge_with" with "#{id}"|
+	step %Q|I press "Merge"|
+end
